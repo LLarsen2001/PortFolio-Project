d36ce77404e54dcebf256b905f16172f61fd2363
Colby Patterson <cpatt@byui.edu>
2022-07-06 17:22:36 -0600

    Added react-router-bootstrap

--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -4,6 +4,7 @@ import { AuthContext } from "../../providers/AuthProvider";
 import Navbar from "react-bootstrap/Navbar"
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const BuildingNavbar = () => {
     const { user, logout } = useContext(AuthContext);
@@ -11,8 +12,9 @@ const BuildingNavbar = () => {
         if (user) {
             return (
                 <>
-                    <Nav.Link href="/account">Account</Nav.Link>
+                    <LinkContainer to="/account"><Nav.Link href="/account">Account</Nav.Link></LinkContainer>
                     <Nav.Link href={logout()}>Logout</Nav.Link>
+                    
                 </>
             );
         } else {
@@ -20,9 +22,13 @@ const BuildingNavbar = () => {
                 <>
 
 
-
+                    <LinkContainer to="/login">
                     <Nav.Link href="/login">Login</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/signup">
                     <Nav.Link href="/signup">Sign Up</Nav.Link>
+                    </LinkContainer>
+                    
 
 
 
@@ -35,10 +41,16 @@ const BuildingNavbar = () => {
         <>
             <Navbar bg="light" variant="light">
                 <Container>
+                    <LinkContainer to="/">
                     <Navbar.Brand href="/home">JobSeek</Navbar.Brand>
+                    </LinkContainer>
                     <Nav className="me-auto">
+                        <LinkContainer to="/">
                         <Nav.Link href="/home">Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/hooks">
                         <Nav.Link href="/hooks">Hooks</Nav.Link>
+                        </LinkContainer>
                         {renderNavItems()}
                     </Nav>
                 </Container>
