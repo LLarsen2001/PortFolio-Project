0a5c0d9d13b8db586f27d420349ff684aab24543
Lance Larsen <lancelarsen2001@gmail.com>
2022-07-07 17:26:12 -0600

    fe pages added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::API
         protected
 
         def configure_permitted_parameters
-                update_attrs = [:name, :password, etc]
-                added_attrs = [:name, :email, :password, etc,]
+                update_attrs = [:name, :password]
+                added_attrs = [:name, :email, :password ]
                 devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
                 devise_parameter_sanitizer.permit :account_update, keys: update_attrs
         end
