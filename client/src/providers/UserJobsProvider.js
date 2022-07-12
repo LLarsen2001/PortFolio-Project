e66984fd07a86ec05ed88c600ce45dae11770008
Lance Larsen <lancelarsen2001@gmail.com>
2022-07-12 15:52:16 -0600

    userjobs FE Provider and board made

---
+++ client/src/providers/UserJobsProvider.js
@@ -0,0 +1,34 @@
+import axios from "axios"
+import React, { useContext, useEffect, useState } from "react"
+
+import { AuthContext } from "./AuthProvider"
+
+export const UserJobsContext = React.createContext();
+
+const UserJobsProvider = ({ children }) => {
+    const [userJobs, setUserJobs] = useState([])
+    const { user } = useContext(AuthContext)
+    console.log(user)
+    useEffect(() => {
+        if (!user) {
+            return
+        }
+        getUserJobs();
+    }, [user]);
+
+    const getUserJobs = async () => {
+        let res = await axios.get(`/api/users/${user.id}/userjobs`)
+        setUserJobs(res.data)
+
+    };
+
+
+
+    return (
+
+        <UserJobsContext.Provider value={{ userJobs }}>
+            {children}
+        </UserJobsContext.Provider>
+    )
+};
+export default UserJobsProvider;
