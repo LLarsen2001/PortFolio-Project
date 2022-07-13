78509269c333cfaaf10660517f81d2fa3fbf618d
Lance Larsen <lancelarsen2001@gmail.com>
2022-07-13 10:41:24 -0600

    card duplecation fixed

--- client/src/components/JobsBoard Components/UserJobCard.js
+++ client/src/components/JobsBoard Components/UserJobCard.js
@@ -5,28 +5,26 @@ import Row from 'react-bootstrap/Row';
 const UserJobCard = (props) => {
 
     return (
-        <Row xs={1} md={2} className="g-4">
-            {Array.from({ length: 4 }).map((_, idx) => (
-                <Col>
-                    <Card key={'primary'} style={{ margin: '15px' }} >
+
+        <Col>
+            <Card key={'primary'} style={{ margin: '15px' }} >
 
 
-                        <Card.Header>Posted by {props.email}</Card.Header>
-                        <Card.Body>
-                            <Card.Title>Job Title: {props.jobname}</Card.Title>
-                            <Card.Text>
-                                <p>Job Description: {props.description}</p>
-                                <p>{props.location}</p>
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            {props.companyname}
+                <Card.Header>Posted by {props.email}</Card.Header>
+                <Card.Body>
+                    <Card.Title>Job Title: {props.jobname}</Card.Title>
+                    <Card.Text>
+                        <p>Job Description: {props.description}</p>
+                        <p>{props.location}</p>
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    {props.companyname}
 
-                        </Card.Footer>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
+                </Card.Footer>
+            </Card>
+        </Col>
+
     );
 }
 export default UserJobCard;
