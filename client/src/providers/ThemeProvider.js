fa16a330640f1c4b8f35e003669fc72937fbb094
Lance Larsen <lancelarsen2001@gmail.com>
2022-07-29 09:28:26 -0600

    update

--- client/src/providers/ThemeProvider.js
+++ client/src/providers/ThemeProvider.js
@@ -6,6 +6,8 @@ const ThemeProvider = ({ children }) => {
   let primary = "#4640DE"
   let secondary = "#CCCCF5"
   let tertiary = "#E7F6FD"
+  let ujSecondColor = "#34A853"
+  let ujThirdColor = "#34A853"
 
   return (
     <ThemeContext.Provider value={{ primary, secondary, tertiary }}>
