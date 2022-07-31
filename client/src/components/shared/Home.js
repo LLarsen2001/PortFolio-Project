ad28e68f49d73457994a5bf893d729218d1163ed
Lance Larsen <lancelarsen2001@gmail.com>
2022-07-31 13:06:42 -0600

    home page added styling

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,9 +1,25 @@
 import React from "react"
+import BackGround from "../../Imgstyle/Desktop.png"
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div style={{ display: 'flex', flexDirection: 'column', }}>
+            <img src={BackGround} style={{ width: "100rem", height: "45rem" }} />
+            <footer style={{ background: 'black', display: 'flex', flexDirection: 'row' }}>
+                <div style={{ display: "flex", flexDirection: 'column', marginLeft: '.5rem', marginRight: "auto", }}>
+
+                    <h4>JobHuntly</h4>
+                    <p5>Great platform for the job seeker that passionate about startups. Find your dream job easier.</p5>
+                </div>
+                <div style={{ display: "flex", flexDirection: 'column', marginLeft: "auto", marginRight: "auto" }}>
+                    <h4>About</h4>
+                    <p5>Team</p5>
+                </div>
+                <div style={{ display: "flex", flexDirection: 'column' }}>
+                    <h4>Resources</h4>
+                    <p5>Conteact Us</p5>
+                </div>
+            </footer>
         </div>
     )
 }
