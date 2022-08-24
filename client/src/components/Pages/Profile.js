a1d78806abd50d1666f98a71e42ad9d5fc3b1484
Colby Patterson <cpatt@byui.edu>
2022-08-24 15:01:34 -0600

    Adjusted styling for no user image

--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -1,111 +1,117 @@
 import React, { useContext, useState } from "react";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
 import { AuthContext } from "../../providers/AuthProvider";
 import styled from "styled-components";
-import Form from "react-bootstrap/Form"
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/esm/Button";
 import SingleImageUpload from "../shared/SingleImageUpload";
 import Job from "./Job";
 import { JobsContext } from "../../providers/JobsProvider";
+import background from "../../Imgstyle/Background.png";
 
 const ImageS = styled.div`
- 
   max-width: 30%;
-  
 `;
 
 const CreatedJobs = styled.div`
-display: flex;
+  display: flex;
 
-align-items: center;
+  align-items: center;
 
-overflow-x: auto;
+  overflow-x: auto;
 `;
 
 const Container = styled.div`
-display: grid;
-float: right;
-max-width: 30%;
+  display: grid;
+  float: right;
+  max-width: 30%;
 `;
 
 const Profile = () => {
-  // const [noImage] = useState("https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.kindpng.com%2Fpicc%2Fm%2F451-4517876_default-profile-hd-png-download.png&imgrefurl=https%3A%2F%2Fwww.kindpng.com%2Fimgv%2FhxibTwh_default-profile-hd-png-download%2F&tbnid=4edWzZTKs1yRvM&vet=12ahUKEwiu48-sk5f5AhXmATQIHQ_qBisQMygdegUIARCkAg..i&docid=xwIlDPGqcBhivM&w=860&h=663&q=default%20profile%20picture&ved=2ahUKEwiu48-sk5f5AhXmATQIHQ_qBisQMygdegUIARCkAg")
-  const { user, setUser, EditUser } = useContext(AuthContext)
-  const { jobs } = useContext(JobsContext)
-  const [emailToggle, setEmailToggle] = useState(false)
-  const [nameToggle, setNameToggle] = useState(false)
-  const [passwordToggle, setPasswordToggle] = useState(true)
-  const [email, setEmail] = useState(user.email || '')
-  const [name, setName] = useState(user.name || '')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
-  const [loading, setLoading] = useState(true)
+  const noImage = background;
+  const { user, setUser, EditUser } = useContext(AuthContext);
+  const { jobs } = useContext(JobsContext);
+  const [emailToggle, setEmailToggle] = useState(false);
+  const [nameToggle, setNameToggle] = useState(false);
+  const [passwordToggle, setPasswordToggle] = useState(true);
+  const [email, setEmail] = useState(user.email || "");
+  const [name, setName] = useState(user.name || "");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [loading, setLoading] = useState(true);
   const edit = true;
 
   const handleEmailToggle = () => {
-    setEmailToggle(!emailToggle)
-  }
+    setEmailToggle(!emailToggle);
+  };
   const handleNameToggle = () => {
-    setNameToggle(!nameToggle)
-  }
+    setNameToggle(!nameToggle);
+  };
   const handlePasswordToggle = () => {
-    setPasswordToggle(!passwordToggle)
-  }
+    setPasswordToggle(!passwordToggle);
+  };
   const handleEmailSubmit = (e) => {
-    e.preventDefault()
-    EditUser({ email, name })
-    setEmailToggle(false)
-    setNameToggle(false)
-  }
+    e.preventDefault();
+    EditUser({ email, name });
+    setEmailToggle(false);
+    setNameToggle(false);
+  };
 
   const handlePasswordSubmit = (p) => {
-    p.preventDefault()
+    p.preventDefault();
     if (password.length < 6) {
-      alert('Password not long enough')
+      alert("Password not long enough");
     }
     if (password !== passwordConfirm) {
-      alert('passwords do not match')
+      alert("passwords do not match");
     }
-    console.log(password)
-    EditUser({ password })
-    setPasswordToggle(false)
-  }
+    console.log(password);
+    EditUser({ password });
+    setPasswordToggle(false);
+  };
 
   const renderPostedJobs = () => {
-    console.log('renderPostedJobs called')
+    console.log("renderPostedJobs called");
     // if (loading) {
     //   return <p>Loading</p>
     // }
-    console.log(jobs)
-    let postedJobs = jobs.filter((j) => j.created_by === user.id)
-    console.log(postedJobs)
-    return postedJobs.map(j => {
-      return (<div className='jobpage'>
-        <Job key={j.id} edit={edit} {...j} />
-      </div>
-      )
-    })
-  }
+    console.log(jobs);
+    let postedJobs = jobs.filter((j) => j.created_by === user.id);
+    console.log(postedJobs);
+    return postedJobs.map((j) => {
+      return (
+        <div className="jobpage">
+          <Job key={j.id} edit={edit} {...j} />
+        </div>
+      );
+    });
+  };
 
   return (
-    <Card style={{
-      width: "auto",
-      height: "auto",
-      boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
-      justifyContent: "center",
-      alignItems: "",
-      margin: "0 auto",
-      float: "none",
-      marginTop: "1rem",
-    }}>
-
+    <Card
+      style={{
+        width: "auto",
+        height: "auto",
+        boxShadow:
+          "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
+        justifyContent: "center",
+        alignItems: "",
+        margin: "0 auto",
+        float: "none",
+        marginTop: "1rem",
+      }}
+    >
       <Card.Body>
         <Container>
-          <Card.Img variant="top" src={user.image ? user.image : "noImage"} />
+          <Card.Img variant="top" src={user.image ? user.image : noImage} />
           <SingleImageUpload id={user.id} setUser={setUser} />
         </Container>
-        <Card.Title style={{ fontFamily: "Poppins", fontSize: "50px" }}>Profile</Card.Title>
-        <Card.Text style={{ fontFamily: "Poppins", fontSize: "25px" }}>Hello {user.name}</Card.Text>
+        <Card.Title style={{ fontFamily: "Poppins", fontSize: "50px" }}>
+          Profile
+        </Card.Title>
+        <Card.Text style={{ fontFamily: "Poppins", fontSize: "25px" }}>
+          Hello {user.name}
+        </Card.Text>
         <Card.Text>{user.email}</Card.Text>
       </Card.Body>
       <div style={{ border: ".5px solid grey " }}></div>
@@ -117,16 +123,19 @@ const Profile = () => {
       <div style={{ border: ".5px solid grey " }}></div>
       <Card.Body>
         <>
-          <Card.Text style={{ fontFamily: "Poppins", fontSize: "20px" }}>Account Information</Card.Text>
+          <Card.Text style={{ fontFamily: "Poppins", fontSize: "20px" }}>
+            Account Information
+          </Card.Text>
           <Form onSubmit={handleEmailSubmit} style={{ width: "30%" }}>
             <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label className="FormLabel">Name: </Form.Label>
               <Form.Control
                 value={name}
                 onChange={(n) => {
-                  setName(n.target.value)
+                  setName(n.target.value);
                 }}
-                type="name" placeholder="Change Username?"
+                type="name"
+                placeholder="Change Username?"
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -134,64 +143,70 @@ const Profile = () => {
               <Form.Control
                 value={email}
                 onChange={(e) => {
-                  setEmail(e.target.value)
+                  setEmail(e.target.value);
                 }}
-                type="email" placeholder="Change Email?"
+                type="email"
+                placeholder="Change Email?"
               />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
             </Button>
           </Form>
-
-
         </>
       </Card.Body>
       <div style={{ border: ".5px solid grey " }}></div>
       <Card.Body>
         <>
-          <Card.Text style={{ fontFamily: "Poppins", fontSize: "20px" }}>Change your Password:</Card.Text>
+          <Card.Text style={{ fontFamily: "Poppins", fontSize: "20px" }}>
+            Change your Password:
+          </Card.Text>
           {/* <Button size="sm" onClick={handlePasswordToggle}>Edit</Button> */}
-          {passwordToggle &&
+          {passwordToggle && (
             <Form onSubmit={handlePasswordSubmit} style={{ width: "30%" }}>
               <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Label className="FormLabel"> Enter new password:</Form.Label>
+                <Form.Label className="FormLabel">
+                  {" "}
+                  Enter new password:
+                </Form.Label>
                 <Form.Control
                   value={password}
                   onChange={(p) => {
-                    setPassword(p.target.value)
+                    setPassword(p.target.value);
                   }}
-                  type="name" placeholder="Change Password"
+                  type="name"
+                  placeholder="Change Password"
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Label className="FormLabel">Confirm New Password: </Form.Label>
+                <Form.Label className="FormLabel">
+                  Confirm New Password:{" "}
+                </Form.Label>
                 <Form.Control
                   value={passwordConfirm}
                   onChange={(p) => {
-                    setPasswordConfirm(p.target.value)
+                    setPasswordConfirm(p.target.value);
                   }}
-                  type="name" placeholder="Confirm New Password"
+                  type="name"
+                  placeholder="Confirm New Password"
                 />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Submit
               </Button>
             </Form>
-          }
-
+          )}
         </>
-
       </Card.Body>
       <div style={{ border: ".5px solid grey " }}></div>
 
       <Card.Body>
-        <Card.Text style={{ fontFamily: "Poppins", fontSize: "20px" }}>My Posted Jobs</Card.Text>
-        <CreatedJobs>
-          {renderPostedJobs()}
-        </CreatedJobs>
+        <Card.Text style={{ fontFamily: "Poppins", fontSize: "20px" }}>
+          My Posted Jobs
+        </Card.Text>
+        <CreatedJobs>{renderPostedJobs()}</CreatedJobs>
       </Card.Body>
     </Card>
   );
-}
+};
 export default Profile;
